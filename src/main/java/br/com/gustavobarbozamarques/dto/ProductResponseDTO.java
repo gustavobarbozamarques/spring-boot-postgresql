973d163757c2667999458bff8958d3aba2fd1d0f
Gustavo Marques <gustavomarques.contato@gmail.com>
2021-06-20 14:18:57 -0300

    Unit test for ProductService.getAll

--- src/main/java/br/com/gustavobarbozamarques/dto/ProductResponseDTO.java
+++ src/main/java/br/com/gustavobarbozamarques/dto/ProductResponseDTO.java
@@ -1,12 +1,16 @@
 package br.com.gustavobarbozamarques.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductResponseDTO {
     private Integer id;
     private String name;
--- src/test/java/br/com/gustavobarbozamarques/services/ProductServiceTest.java
+++ src/test/java/br/com/gustavobarbozamarques/services/ProductServiceTest.java
@@ -1,12 +1,44 @@
 package br.com.gustavobarbozamarques.services;
 
+import br.com.gustavobarbozamarques.config.ModelMapperConfig;
+import br.com.gustavobarbozamarques.mocks.ProductMock;
+import br.com.gustavobarbozamarques.repositories.CategoryRepository;
+import br.com.gustavobarbozamarques.repositories.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class ProductServiceTest {
 
-    @Test
-    public void test() {
+    @Mock
+    private CategoryRepository categoryRepository;
 
+    @Mock
+    private ProductRepository productRepository;
+
+    private ProductService productService;
+
+    @BeforeEach
+    public void setup() {
+        var modelMapper = new ModelMapperConfig().modelMapper();
+        productService = new ProductService(categoryRepository, productRepository, modelMapper);
+    }
+
+    @Test
+    public void testGetAll() {
+        var productsFromDatabase = List.of(ProductMock.get());
+        when(productRepository.findAll()).thenReturn(productsFromDatabase);
+        var products = productService.getAll();
+        assertThat(products)
+                .isNotEmpty()
+                .hasSize(productsFromDatabase.size());
     }
 }
